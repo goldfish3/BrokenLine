adf4769ac71587f467c6b7883c2e09766536e0b6
goldfish3 <goldfish3@163.com>
2017-04-05 00:42:54 +0800

    又加了点儿注释

--- BrokenLineForGit/折线图/ViewController.m
+++ BrokenLineForGit/折线图/ViewController.m
@@ -16,6 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    //
     BrokenLine *brokenLine = [[BrokenLine alloc] initWithFrame:CGRectMake(0,100, self.view.frame.size.width, 100)];
     [self.view addSubview:brokenLine];
 }
